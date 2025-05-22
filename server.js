85535678f352f1015bb8074f752d09e3b1abb39c
Ian Shapiro <ianshapiro1@gmail.com>
2025-05-22 00:31:42 -0500

    rate limiting, CSP

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import Fastify from "fastify";
+import fastifyStatic from "@fastify/static";
+import rateLimit from "@fastify/rate-limit";
+import fastifyHelmet from "@fastify/helmet";
+
 import path from "path";
 import { fileURLToPath } from "url";
-import fastifyStatic from "@fastify/static";
 import dotenv from "dotenv";
+
 import shoutPlugin from "./api/shout.js";
+
+
 
 dotenv.config();
 
@@ -11,6 +17,30 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const fastify = Fastify({ logger: true });
+
+//helmet with CSP
+await fastify.register(fastifyHelmet, {
+  contentSecurityPolicy: {
+    useDefaults: true,
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'"],
+      styleSrc: ["'self'"],
+      imgSrc: ["'self'", "data:"],
+      objectSrc: ["'none'"],
+      baseUri: ["'self'"],
+      frameAncestors: ["'none'"],
+    }
+  }
+});
+
+//rate limiting (basic logic for now)
+await fastify.register(rateLimit, {
+  max: 100,
+  timeWindow: "1 minute",
+  ban: 1,
+});
+
 
 fastify.register(fastifyStatic, {
     root: path.join(__dirname, "public"),
